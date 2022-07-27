7bb5f1cb57cc18dc20fdde4e15465dee6992953a
Thiago <thiagofang@gmail.com>
2022-07-27 19:49:08 -0300

    criando sistema para adicionar poucos inimigos com pouco código e melhorando como o npc seguia os waypoints marcados

--- js/classes.js
+++ js/classes.js
@@ -4,7 +4,11 @@ class Enemy {
         this.position = position;
         this.width = 100;
         this.height = 100;
-        this.waypointIndex = 0
+        this.waypointIndex = 0;
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
     };
 
     draw() {
@@ -21,16 +25,23 @@ class Enemy {
         this.draw();
         
         const waypoint = waypoints[this.waypointIndex]
-        const yDistance = waypoint.y - this.position.y;
-        const xDistance = waypoint.x - this.position.x;
+        const yDistance = waypoint.y - this.center.y;
+        const xDistance = waypoint.x - this.center.x;
         const angle = Math.atan2(yDistance, xDistance);
         
         this.position.x += Math.cos(angle);
         this.position.y += Math.sin(angle);
-
-        if(Math.round(this.position.x) === Math.round(waypoint.x) && 
-        Math.round(this.position.y) === Math.round(waypoint.y)) {
-            this.waypointIndex++
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
         }
+        
+        if(
+            Math.round(this.center.x) === Math.round(waypoint.x) && 
+            Math.round(this.center.y) === Math.round(waypoint.y) &&
+            this.waypointIndex < waypoints.length - 1
+        ) {
+            this.waypointIndex++
+        } 
     };
 };
--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ canvas.height = 768;
 context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-
-
 // Draw
 const image = new Image('./assets/gameMap.png');
 image.onload = () => {
@@ -17,18 +15,21 @@ image.onload = () => {
 };
 image.src = ('./assets/gameMap.png');
 
-const enemy = new Enemy({
-    position: {x: 200, y: 200},
-});
-const enemy2 = new Enemy({
-    position: {x: 0, y: 200},
-});
+const enemies = []
+
+for (let i = 1; i <= 10; i++) {
+    const xOffset = i * 200
+    enemies.push(new Enemy({ 
+        position: {x: waypoints[0].x - xOffset, y: waypoints[0].y} 
+    }));
+};
 
 const animate = () => {
     requestAnimationFrame(animate);
 
     context.drawImage(image, 0, 0);
-    enemy.update();
-    enemy2.update();
+    enemies.forEach(enemy => {
+        enemy.update();
+    });
 };
 animate();
